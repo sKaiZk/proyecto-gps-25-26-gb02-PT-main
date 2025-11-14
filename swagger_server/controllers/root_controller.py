83053cf996d10012a6142aa2a383bf7fd29e2ea3
Driox07 <82775672+Driox07@users.noreply.github.com>
2025-11-14 14:05:17 +0100

    Terminada funcionalidad básica de PT. Añadido inicio con info.

---
+++ swagger_server/controllers/root_controller.py
@@ -0,0 +1,11 @@
+from typing import List
+from swagger_server.models.error import Error  # noqa: E501
+import requests
+"""
+controller generated to handled auth operation described at:
+https://connexion.readthedocs.io/en/latest/security.html
+"""
+
+def get_root():
+    """Gets root"""
+    return "Proveedor de Tracks (PT) MSVC - GB02 - Samuel Monasterio Pérez"
